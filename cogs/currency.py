e3b156c43d27d9dd10c6ac1d8b89a9df97b2b091
GPM567 <hwangminki1010@gmail.com>
2020-08-26 11:29:41 +0900

    currency.py

--- cogs/currency.py
+++ cogs/currency.py
@@ -5,6 +5,10 @@ class Currency(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="지갑")
+    async def wallet(self, ctx: commands.Context):
+        await ctx.send("test")
+
 
 def setup(bot):
     bot.add_cog(Currency(bot))

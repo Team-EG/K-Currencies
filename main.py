1c3849df274f0f43298d1c967e8b95eb99886f7e
eunwoo1104 <sions04@naver.com>
2020-08-26 11:37:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ import os
 releaseToken = "Njk3Njc2NTcwNDI4NDQwNTg2.XsSIpA.YWkZZ9g25ybZn8HvjqRkcgzBZhI"
 canaryToken = "NzAzMjQwOTg4NjI5NjYzNzg0.XqLuPA.Uvmj20wkag0s3oCEvfSJpWg2UMk"
 
-bot = commands.Bot(command_prefix="!CM ", help_command=None)
+
+async def get_prefix(bot, message):
+    return commands.when_mentioned_or("!CM ")(bot, message)
+
+
+bot = commands.Bot(command_prefix=get_prefix, help_command=None)
 
 isRelease = False
 
 releasePassFiles = []
 canaryPassFiles = []
 
-for filename in os.listdir("./cogs"):
-    if isRelease:
-        passFile = releasePassFiles
-    else:
-        passFile = canaryPassFiles
-    if not filename in passFile:
-        if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{filename[:-3]}')
+if isRelease:
+    passFile = releasePassFiles
+else:
+    passFile = canaryPassFiles
+
+[bot.load_extension(f"cogs.{x.replace('.py', '')}") for x in os.listdir("./cogs") if x.endswith('.py') and x not in passFile]
 
 if isRelease:
     token = releaseToken

69c48db32b366687011b1a7ec347a4285c491736
GPM567 <62422715+GPM567@users.noreply.github.com>
2021-02-11 19:00:57 +0900

    .

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from cogs import *
 from modules import accessToDB
 
 tokens = {
-    "release": "NzUyMzU0NDMzMTA2NzA2NDUy.X1Waqg.UcEeM0OG-SJSU8BdPTElDIipudc",
-    "beta": "NzUyNzE1NzE4OTk4MzYwMDk0.X1brJA.qp1S7trqFN0YEqKv6CBl_tLj60Y"
+    "release": "token", "canary": "token"
 }
 
 
@@ -17,13 +16,7 @@ async def get_prefix(bot, message):
 
 bot = commands.Bot(command_prefix=get_prefix, help_command=None, intents=discord.Intents.all())
 
-dbkrpy.UpdateGuilds(bot=bot, token="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc"
-                                   "1MjM1NDQzMzEwNjcwNjQ1MiIsImlhdCI6MTU5OTYxMTc5Miw"
-                                   "iZXhwIjoxNjMxMTY5MzkyfQ.Y_Gl_uspPk-xZ3R3jl7-HoCX"
-                                   "TuNHK2icKuFV6DWVggm6OD4QuG6OQLZdb_tnLhTGHk3MzcNX"
-                                   "KtEKsXFIHjcun3QpYb3rRrVHjLDu-2mYAOoUxoLZimnoPAGR"
-                                   "p9c8SCzxVV3BiGPby2ckMjj6Zycra8OwELZYKb1RAYtujTMAdCE")
-
+dbkrpy.UpdateGuilds(bot=bot, token="token")
 
 mode = "release"  # release or beta
 
